3b602848651f3f15bdd0c23e9dd861cc64010ffd
mva-odoo <mva@odoo.com>
2024-09-20 16:02:27 +0200

    Exercice 1

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Open Academy",
+    "version": "1.0",
+    "author": "MVannerom",
+    "depends": ["base"],
+    "website": "https://www.odoo.com",
+    "data": [],
+    "license": "OEEL-1",
+}
+
